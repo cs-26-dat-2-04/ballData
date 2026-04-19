5541bd57215e4967b3fa75c2a3d1e329a650e373
Anders Balleby <andersballeby42@gmail.com>
2026-04-19 20:51:55 +0200

    Added a testing endpoint for auth

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import authRouter from "./routes/auth.js";
+import auth from "./middleware/auth.js";
 
 dotenv.config();
 
@@ -13,7 +14,10 @@ app.use(cors({ origin: "http://localhost:3000" }));
 app.use(express.json());
 
 // Health endpoint - til at tjekke om serveren kører
-app.get("/health", (_req, res) => res.json({ status: "ok" }));
+app.get("/health", async (_req, res) => res.json({ status: "ok" }));
+
+// Til at teste auth
+app.get("/health_auth", auth, async (_req, res) => res.json({ status: "ok" }));
 
 // Auth routes
 app.use("/auth", authRouter);
