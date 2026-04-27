26d574802da7de3bfc5dc8be4f58c0887923779a
Anders Balleby <andersballeby42@gmail.com>
2026-04-27 07:58:33 +0200

    Minor formatting changes

--- frontend/src/components/SeasonGraph.jsx
+++ frontend/src/components/SeasonGraph.jsx
@@ -1,75 +1,82 @@
-'use client';  // ← must be the first line
+"use client"; // ← must be the first line
 
 import styles from "../components/styles.module.css";
-import {Bar} from 'react-chartjs-2';
+import { Bar } from "react-chartjs-2";
 
-import { 
-    Chart as ChartJS,
-    CategoryScale,
-    LinearScale,
-    BarElement,
-    Title,
-    Tooltip,
-    Legend
-} from 'chart.js'
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
 
 ChartJS.register(
-    CategoryScale,
-    LinearScale,
-    BarElement,
-    Title,
-    Tooltip,
-    Legend
-)
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+);
 
-export default function SeasonGraph({labels, data, bgcolor, bdcolor, title}) {
+export default function SeasonGraph({ labels, data, bgcolor, bdcolor, title }) {
+  const chartData = {
+    labels: labels,
+    datasets: [
+      {
+        data: data,
+        borderColor: bdcolor,
+        backgroundColor: bgcolor,
+        borderWidth: 1,
+        borderRadius: 10,
+        barThickness: 68,
+        hoverBackgroundColor: bgcolor,
+      },
+    ],
+  };
 
-    const chartData = {
-            labels: labels,
-            datasets: [
-                {
-                    data: data,
-                    borderColor: bdcolor,
-                    backgroundColor: bgcolor,
-                    borderWidth: 1,
-                    borderRadius: 10,
-                    barThickness: 68,
-                    hoverBackgroundColor: bgcolor
-                }
-            ]
-        };
+  const chartOptions = {
+    plugins: {
+      legend: {
+        display: false,
+      },
+      title: {
+        display: false,
+      },
+      maintainAspectRatio: false,
+      responsive: true,
+    },
+  };
 
-    const chartOptions = {
-            plugins: {
-                legend: {
-                    display: false
-                },
-                title: {
-                    display: false,
-                },
-            maintainAspectRatio: false,
-            responsive: true,
-            }
-    }
-
-    return(
-        <>
-        <article className={styles.graphCard}>
-                <p className={styles.graphTitle}>{title ?? "-"}</p>
-                <div style={{height:"60%", display:"flex", justifyContent:"center"}}>
-                    <Bar style={{float:"center"}} data={chartData} width={100} height={100} options={chartOptions}/>
-                </div>
-                <div className={styles.graphNumbersContainer}>
-                    <a style={{color:"#1D9E75"}}>{data[0]}</a>
-                    <a style={{color:"#E24B4A"}}>{data[1]}</a>
-                    <a style={{color:"#9AA0AD"}}>{data[2]}</a>
-                </div>
-                <div className={styles.graphDescriptorsContainer}>
-                    <a>Sejre</a>
-                    <a>Nederlag</a>
-                    <a>Uafgjort</a>
-                </div>
-        </article>
-        </>
-    )
+  return (
+    <>
+      <article className={styles.graphCard}>
+        <p className={styles.graphTitle}>{title ?? "-"}</p>
+        <div
+          style={{ height: "60%", display: "flex", justifyContent: "center" }}
+        >
+          <Bar
+            style={{ float: "center" }}
+            data={chartData}
+            width={100}
+            height={100}
+            options={chartOptions}
+          />
+        </div>
+        <div className={styles.graphNumbersContainer}>
+          <a style={{ color: "#1D9E75" }}>{data[0]}</a>
+          <a style={{ color: "#E24B4A" }}>{data[1]}</a>
+          <a style={{ color: "#9AA0AD" }}>{data[2]}</a>
+        </div>
+        <div className={styles.graphDescriptorsContainer}>
+          <a>Sejre</a>
+          <a>Nederlag</a>
+          <a>Uafgjort</a>
+        </div>
+      </article>
+    </>
+  );
 }
--- frontend/src/components/StatCard.jsx
+++ frontend/src/components/StatCard.jsx
@@ -1,18 +1,34 @@
 import styles from "../components/styles.module.css";
 import Image from "next/image";
 
-export default function StatCard({ title, body, footer, iconColor, icon, iconAlt = "" }) {
+export default function StatCard({
+  title,
+  body,
+  footer,
+  iconColor,
+  icon,
+  iconAlt = "",
+}) {
   // The backend will insert the values into the parameters.
   return (
     // We are currently using placeholders until we link frontend to backend
     <article className={styles.card}>
-      <div style={{display:"flex"}}>
+      <div style={{ display: "flex" }}>
         <div>
           <span className={styles.cardTitle}>{title}</span>
-          <p className={styles.cardBody}>{body ?? "-"}</p>  
+          <p className={styles.cardBody}>{body ?? "-"}</p>
         </div>
-          <div style={{marginLeft:"auto", marginRight:"25px", alignSelf:"center"}}>
-            <div className={styles.iconContainer} style={{backgroundColor:iconColor}}>
+        <div
+          style={{
+            marginLeft: "auto",
+            marginRight: "25px",
+            alignSelf: "center",
+          }}
+        >
+          <div
+            className={styles.iconContainer}
+            style={{ backgroundColor: iconColor }}
+          >
             {icon && <Image src={icon} width={64} height={64} alt={iconAlt} />}
           </div>
         </div>
