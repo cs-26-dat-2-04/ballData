b8b3f4e8db2ad7332fcd54bdb6754089751457df
Anders Balleby <andersballeby42@gmail.com>
2026-05-04 15:33:11 +0200

    Added /signup route

--- frontend/src/app/signup/page.jsx
+++ frontend/src/app/signup/page.jsx
@@ -1,33 +1,52 @@
 import "./StyleLogin.css";
 import React from "react";
-import Head from 'next/head';
+import Head from "next/head";
 
-
-export default function CoachLogin() {
+export default function CoachSignup() {
   return (
-<>
-    <title>Opret Bruger</title>
-    <div className="wrapper">
+    <>
+      <title>Opret Bruger</title>
+      <div className="wrapper">
         <Head>
-            <title>Opret Bruger</title>
+          <title>Opret Bruger</title>
         </Head>
         <h1>Opret Bruger</h1>
         <form>
-            <div>
-                <input type="firstname" name="firstname" id="fistname-input" placeholder="Fornavn"/>
-            </div>
-            <div>
-                <input type="email" name="email" id="email-input" placeholder="Email"/>
-            </div>
-            <div>
-                <input type="password" name="password" id="password-input" placeholder="Password"/>
-            </div>
-            <div>
-                <input type="password" name="repeat-password" id="repeat-password-input" placeholder="Gentag Password"/>
-            </div>
-            <button type="submit">Opret Bruger</button>
+          <div>
+            <input
+              type="firstname"
+              name="firstname"
+              id="fistname-input"
+              placeholder="Fornavn"
+            />
+          </div>
+          <div>
+            <input
+              type="email"
+              name="email"
+              id="email-input"
+              placeholder="Email"
+            />
+          </div>
+          <div>
+            <input
+              type="password"
+              name="password"
+              id="password-input"
+              placeholder="Password"
+            />
+          </div>
+          <div>
+            <input
+              type="password"
+              name="repeat-password"
+              id="repeat-password-input"
+              placeholder="Gentag Password"
+            />
+          </div>
+          <button type="submit">Opret Bruger</button>
         </form>
-    </div>
-</>
-  )
-};
+      </div>
+    </>
+  );
+}
