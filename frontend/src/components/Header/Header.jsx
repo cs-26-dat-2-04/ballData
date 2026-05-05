23bac71ba45492fc565acfdcf537059f56bb27c1
mikkelj <JZ34PU@student.aau.dk>
2026-05-05 08:56:30 +0200

    Sidst opdateret fjernet og rykket Hold og Kampe (#74)

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -12,17 +12,12 @@ export default function Header() {
           <a style={{ color: "var(--muted)" }}>Sæson {currentSeasonString()}</a>
         </div>
         
-        <Link href={"/teams"} className={styles.navLink} style={{borderRight:"2px solid var(--rule)"}}> Hold</Link>
+          <nav className={styles.headerNav}>
+           <Link href={"/teams"} className={styles.navLink} style={{borderRight:"2px solid var(--rule)"}}> Hold</Link>
         
         
         <Link href={"/matches"} className={styles.navLink}> Kampe</Link>
-        
-        <div style={{ marginLeft: "auto", alignSelf: "center" }}>
-          <a style={{ color: "var(--hint)" }}>
-            Sidst opdateret: {updateTime(new Date())}
-          </a>{" "}
-          {/*new date() so far, but needs to be changed when data actually gets updated*/}
-        </div>
+          </nav>
       </header>
     </>
   );
@@ -35,29 +30,4 @@ function currentSeasonString() {
   return 8 <= currentDate.getMonth()
     ? `${currentDate.getFullYear()}/${currentDate.getFullYear() + 1}`
     : `${currentDate.getFullYear() - 1}/${currentDate.getFullYear()}`;
-}
-
-function updateTime(updateDate) {
-  const weekDays = [
-    "Mandag",
-    "Tirsdag",
-    "Onsdag",
-    "Torsdag",
-    "Fredag",
-    "Lørdag",
-    "Søndag",
-  ];
-  const currentDate = new Date();
-  let updateDateString =
-    updateDate.getDay() === currentDate.getDay()
-      ? "I dag"
-      : weekDays[updateDate.getDay()];
-
-  let updateTimeString = `${updateDate.getHours() + updateDate.getTimezoneOffset() / 60}:`;
-  updateTimeString +=
-    updateDate.getMinutes() <= 9
-      ? `0${updateDate.getMinutes()}`
-      : updateDate.getMinutes();
-
-  return `${updateDateString}, ${updateTimeString}`;
 }
