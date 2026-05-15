f8ef90e5b4a62c0c70ef4793d0abf072e70d489d
AndersBalleby <andersballeby42@gmail.com>
2026-05-15 09:14:37 +0200

    stopid

--- backend/src/controllers/auth.js
+++ backend/src/controllers/auth.js
@@ -4,7 +4,6 @@ const COOKIE_OPTIONS = {
   httpOnly: true,
   secure: process.env.NODE_ENV === "production",
   sameSite: "none",
-  sameSite: "lax",
   maxAge: 7 * 24 * 60 * 60 * 1000, // 7 dage
 };
 
