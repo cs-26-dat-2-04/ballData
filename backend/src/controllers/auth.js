9a3bdb595547b8636341046cf30db7253bf852e4
AndersBalleby <andersballeby42@gmail.com>
2026-05-15 09:10:11 +0200

    cors changes

--- backend/src/controllers/auth.js
+++ backend/src/controllers/auth.js
@@ -3,6 +3,7 @@ import * as authService from "../services/auth.js";
 const COOKIE_OPTIONS = {
   httpOnly: true,
   secure: process.env.NODE_ENV === "production",
+  sameSite: "none",
   sameSite: "lax",
   maxAge: 7 * 24 * 60 * 60 * 1000, // 7 dage
 };
