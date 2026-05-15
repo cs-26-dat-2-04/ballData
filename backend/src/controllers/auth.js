ca29530713e3afe292afef2dd65ac9e2b2da315a
AndersBalleby <andersballeby42@gmail.com>
2026-05-15 13:23:22 +0200

    added custom dns

--- backend/src/controllers/auth.js
+++ backend/src/controllers/auth.js
@@ -3,7 +3,8 @@ import * as authService from "../services/auth.js";
 const COOKIE_OPTIONS = {
   httpOnly: true,
   secure: process.env.NODE_ENV === "production",
-  sameSite: "none",
+  sameSite: "lax",
+  domain: ".ballebysoftware.dk",
   maxAge: 7 * 24 * 60 * 60 * 1000, // 7 dage
 };
 
