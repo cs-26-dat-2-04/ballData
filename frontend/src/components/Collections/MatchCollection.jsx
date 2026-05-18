833ce9596399e5cb5041f2cddc094c5fd41772e7
FedKat <nicklasmoestrup@gmail.com>
2026-05-18 14:33:15 +0200

    finished

--- frontend/src/components/Collections/MatchCollection.jsx
+++ frontend/src/components/Collections/MatchCollection.jsx
@@ -4,23 +4,7 @@ import Image from "next/image";
 import MatchRow from "../Rows/MatchRow.jsx";
 
 export default function MatchCollection({ data }) {
-  const formatter = new Intl.DateTimeFormat('en-GB', {
-    day: '2-digit',
-    month: '2-digit',
-    year: 'numeric',
-    hour: '2-digit',
-    minute: '2-digit',
-    hour12: false 
-  });
   
-  for (let d of data) {
-    let date
-    try { //I don't even know man, two pieces of invisible data that only exists here and not on /matches that can't be parsed into date, this fixes it
-      date = new Date(d.match_date);
-      d.match_date = formatter.format(date);
-    }
-    catch{}
-  }
   return (
     <article className={collectionStyles.collCard}>
       <div style={{ marginBottom: "-15px" }} className={styles.container}>
