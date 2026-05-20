1eaa8d1a5d595ac009c61463a832ced6c16c5a17
Tsla25 <yo11at@student.aau.dk>
2026-05-20 05:17:05 +0200

    test

--- frontend/src/app/dashboard/matches/[[...id]]/page.jsx
+++ frontend/src/app/dashboard/matches/[[...id]]/page.jsx
@@ -2,7 +2,7 @@
 
 import Link from "next/link";
 import { useState } from "react";
-import styles from "./matchstats.module.css";
+import styles from "./matches.module.css";
 
 import MatchPageClient from "../../../../components/PageClient/MatchPageClient.jsx";
 import { redirect } from "next/navigation";
