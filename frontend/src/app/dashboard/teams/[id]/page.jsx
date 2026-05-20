8c887224ba1886b2368a5224b968b7c500bf0e36
FedKat <nicklasmoestrup@gmail.com>
2026-05-21 01:08:30 +0200

    slettet console.log af teams

--- frontend/src/app/dashboard/teams/[id]/page.jsx
+++ frontend/src/app/dashboard/teams/[id]/page.jsx
@@ -24,7 +24,7 @@ export default async function Teams({ params }) {
   } catch (err) {
     error = err.message;
   }
-  console.log(team);
+  
   return (
     <>
       <title>Hold — Spillere</title>
